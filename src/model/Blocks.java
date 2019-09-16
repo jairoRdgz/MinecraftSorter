07082b3be2db614989994d1bbc382f4b597a64a5
L3C74M0 <juandavidlectamocaicedo@gmail.com>
2019-09-15 21:40:44 -0500

    Ctrl + F

--- src/model/Blocks.java
+++ src/model/Blocks.java
@@ -4,33 +4,37 @@ public class Blocks {
 
 	private int tag;
 	private int quantify;
-	
+
 	public Blocks(int tag, int quantify) {
 		this.tag = tag;
 		this.quantify = quantify;
 	}
+
 	public void addBlocks(Blocks block) {
 		int temp = quantify;
-		if(temp+block.getQuantify()<64) {
-			quantify = quantify+block.getQuantify();
-			System.out.println("monto:"+quantify);
+		if (temp + block.getQuantify() < 64) {
+			quantify = quantify + block.getQuantify();
+			System.out.println("monto:" + quantify);
 			block.setQuantify(0);
-		}else {
+		} else {
 			int diference = quantify - 64;
 			quantify += diference;
-			block.setQuantify(block.getQuantify()-diference);
+			block.setQuantify(block.getQuantify() - diference);
 		}
 	}
-	
+
 	public int getTag() {
 		return tag;
 	}
+
 	public void setTag(int tag) {
 		this.tag = tag;
 	}
+
 	public int getQuantify() {
 		return quantify;
 	}
+
 	public void setQuantify(int quantify) {
 		this.quantify = quantify;
 	}
--- src/model/MineHash.java
+++ src/model/MineHash.java
@@ -7,8 +7,8 @@ import java.util.Queue;
 //import java.util.Hashtable;
 
 public class MineHash {
-	
-	public  static void main(String[]args) {
+
+	public static void main(String[] args) {
 		Blocks one = new Blocks(1, 5);
 		Blocks two = new Blocks(1, 5);
 		Blocks three = new Blocks(1, 5);
@@ -29,32 +29,34 @@ public class MineHash {
 		f.addBlock(seven);
 		f.addBlock(egith);
 		f.addBlock(nine);
-		for(int i = 1; i< 8 ;i++) {
-			Blocks generic = new Blocks(3+i, 5);
+		for (int i = 1; i < 8; i++) {
+			Blocks generic = new Blocks(3 + i, 5);
 			f.addBlock(generic);
 		}
 		Blocks lol = new Blocks(5, 5);
 		f.addBlock(lol);
-		//one.addBlocks(two);
+		// one.addBlocks(two);
 //		f.show();
 //		System.out.println(one.getQuantify());
 //		System.out.println(f.getInventory().get(0).peek().getQuantify());
 //		f.show();
-		for (int i = 0; i < 9; i++){
+		for (int i = 0; i < 9; i++) {
 			Blocks temp = f.getInventory().get(0).poll();
 			System.out.println(temp.getTag());
 			System.out.println(temp.getQuantify());
 			f.getInventory().get(0).add(temp);
 		}
-		//System.out.println(f.getInventory().get(0).peek().getTag());
+		// System.out.println(f.getInventory().get(0).peek().getTag());
 	}
+
 	private Hashtable<Integer, Queue<Blocks>> inventory;
 
-	public MineHash(){
+	public MineHash() {
 		init();
 	}
-	public void init(){
-		inventory = new Hashtable<Integer,Queue<Blocks>>();
+
+	public void init() {
+		inventory = new Hashtable<Integer, Queue<Blocks>>();
 		Queue<Blocks> queue0 = new LinkedList<Blocks>();
 		Queue<Blocks> queue1 = new LinkedList<Blocks>();
 		Queue<Blocks> queue2 = new LinkedList<Blocks>();
@@ -64,39 +66,40 @@ public class MineHash {
 		inventory.put(2, queue2);
 		inventory.put(3, queue3);
 	}
-	public void addBlock(Blocks block){
+
+	public void addBlock(Blocks block) {
 		boolean add = false;
-		boolean flag ;
-		Blocks temp ;
+		boolean flag;
+		Blocks temp;
 		for (int i = 0; i <= 3 && add == false; i++) {
 			flag = false;
 			System.out.println("HASH");
-			for (int j = 0; j <= 8 && flag == false;j++){
+			for (int j = 0; j <= 8 && flag == false; j++) {
 				System.out.println("QUEUE");
-				System.out.println("size:"+inventory.get(0).size());
-				System.out.println("tag:"+block.getTag());
-				System.out.println("quantify:"+block.getQuantify());
-				System.out.println("time:"+j);
-				if(inventory.get(i).size() == 0) {
+				System.out.println("size:" + inventory.get(0).size());
+				System.out.println("tag:" + block.getTag());
+				System.out.println("quantify:" + block.getQuantify());
+				System.out.println("time:" + j);
+				if (inventory.get(i).size() == 0) {
 					inventory.get(i).add(block);
 					flag = true;
 					add = true;
-					//System.out.println("f");
-				}else if(inventory.get(i).peek().getTag()==block.getTag()){
-					if(inventory.get(i).peek().getQuantify()<64){
+					// System.out.println("f");
+				} else if (inventory.get(i).peek().getTag() == block.getTag()) {
+					if (inventory.get(i).peek().getQuantify() < 64) {
 						inventory.get(i).peek().addBlocks(block);
-						if(block.getQuantify()==0) {
-							flag= true;
+						if (block.getQuantify() == 0) {
+							flag = true;
 							add = true;
 							System.out.println("entry for 0");
 
-						}else {
+						} else {
 //							flag = true;
-							temp =  inventory.get(i).poll();
+							temp = inventory.get(i).poll();
 							inventory.get(i).add(temp);
 						}
-					}					
-				}else if(j>inventory.get(i).size()){
+					}
+				} else if (j > inventory.get(i).size()) {
 					inventory.get(i).add(block);
 					flag = true;
 					add = true;
@@ -108,8 +111,8 @@ public class MineHash {
 //					System.out.println("entry");
 //
 //				}
-				else{
-					temp =  inventory.get(i).poll();
+				else {
+					temp = inventory.get(i).poll();
 					inventory.get(i).add(temp);
 //					System.out.println("entry");
 
@@ -117,29 +120,31 @@ public class MineHash {
 			}
 		}
 	}
-	public LinkedList<Blocks> quickAcces(Blocks block){
+
+	public LinkedList<Blocks> quickAcces(Blocks block) {
 		LinkedList<Blocks> retu = new LinkedList<Blocks>();
-		
+
 		return retu;
 	}
-	
-	public void show(){
+
+	public void show() {
 		Blocks temp;
 		for (int i = 0; i <= 3; i++) {
-			for (int j = 1; j <= 8 ;j++){
+			for (int j = 1; j <= 8; j++) {
 				System.out.println(inventory.get(i).peek().getQuantify());
 				temp = inventory.get(i).poll();
 				inventory.get(i).add(temp);
 			}
-			
+
 		}
-		
+
 	}
+
 	public Hashtable<Integer, Queue<Blocks>> getInventory() {
 		return inventory;
 	}
+
 	public void setInventory(Hashtable<Integer, Queue<Blocks>> inventory) {
 		this.inventory = inventory;
 	}
-	
 }
